8a8ebcd7871a015569fa963d8b9648d1705fdba4
oujirate <oujireflect.me@gmail.com>
2024-05-19 22:05:50 +0700

    memperbaiki read_data

--- model.py
+++ model.py
@@ -1,13 +1,16 @@
 import psycopg2
 import connection as condb
 
-def read_data_all(cur,table):
-    query = f"SELECT * FROM {table}"
+def read_data(cur,select = "*", table = "", orderby = ""):
+    if orderby != "":
+        query = f"SELECT {select} FROM {table} ORDER BY {orderby}"
+    else:
+        query = f"SELECT {select} FROM {table}"
     cur.execute(query)
     data = cur.fetchall()
     for i in data:
         print(i)
 
 conn, cur = condb.connect()
-read_data_all(cur,"tipe_kamar")
+read_data(cur,select="id_tipe_kamar, harga",table="tipe_kamar",orderby="harga")
 condb.close(conn, cur)
