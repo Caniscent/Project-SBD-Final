371b33cdaa92b472bda228d3da9a28e2e5ba4ff8
oujirate <oujireflect.me@gmail.com>
2024-05-20 13:32:21 +0700

    Perbaikan close

--- model.py
+++ model.py
@@ -11,12 +11,14 @@ def read_data(select = "*", table = "", orderby = ""):
     data = cur.fetchall()
     for i in data:
         print(i)
+    cur.close()
+    conn.close()
 
     # Penulisan values menggunakan list. Ex: Values = ["data1",data2, "data3"]
     # Harus urut sesuai kolom di database
 def create_data(table,values):
     conn, cur = connection.connect()
-    column = column_data(cur=cur, table=table)
+    column = column_data(table=table)
 
     query_values = []
     for i in values:
@@ -32,6 +34,8 @@ def create_data(table,values):
     # return query
     cur.execute(query,tuple(values))
     conn.commit()
+    cur.close()
+    conn.close()
 
 def update_data():
     pass
@@ -58,12 +62,12 @@ def column_data(table,idenable=0):
 
 # Menambah data
 conn, cur = connection.connect()
-# print("Data Saat ini: ")
-# read_data(table="fasilitas")
-# new_nama_fasilitas = str(input("Nama Fasilitas: "))
-# new_id_jenis_fasilitas = int(input("ID Fasilitas(1/2): "))
-# values = [new_nama_fasilitas,new_id_jenis_fasilitas]
-# create_data(cur,table = "fasilitas", values= values)
+print("Data Saat ini: ")
+read_data(table="fasilitas")
+new_nama_fasilitas = str(input("Nama Fasilitas: "))
+new_id_jenis_fasilitas = int(input("ID Fasilitas(1/2): "))
+values = [new_nama_fasilitas,new_id_jenis_fasilitas]
+create_data(table = "fasilitas", values= values)
 read_data(table="fasilitas")
 cur.close()
 conn.close()
