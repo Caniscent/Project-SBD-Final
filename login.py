f15e321ddbc2d10d52433ca7d9fbba19d1ad844b
ChemicalSpark <rangga.pramudya23@gmail.com>
2024-05-20 23:06:49 +0700

    login sudah di pisah antara owner dan atmin

--- login.py
+++ login.py
@@ -2,6 +2,7 @@ import core
 import getpass
 import hashlib
 import model
+import main
 
 SESSION_GLOBAL = {}
    
@@ -10,10 +11,10 @@ def login():
     #Temp variabel
     data_admin = []
     # Membaca database data_admin
-    data = model.read_data(select="username, password",table="users")
+    data = model.read_data(select="username, password, jenis_users_id",table="users")
     # Convert database csv ke dictionary
     for i in data:
-        data_admin.append({"username": i[0], "password": i[1]})
+        data_admin.append({"username": i[0], "password": i[1], "jenis_users_id": i[2]})
 
     #Count kesempatan untuk login
     percobaan = 3
@@ -34,10 +35,9 @@ def login():
 
         # Validasi input username dan password terhadap database
         for user in data_admin:
-            # Kondisi benar jika input sesuai dengan database
-            if username == user['username'] and hashed_password == user['password']:
+            if user['jenis_users_id'] == 1 and username == user['username'] and hashed_password == user['password']:
                 # UI Login Berhasil
-                salam = f"Selamat Datang {username.title()}"
+                salam = f"Selamat Datang Owner {username.title()}"
                 print('+' + '='*83 + '+')
                 print('|' + '[ LOGIN SUCCESSFUL ]'.center(83) + '|')
                 print('|' + salam.center(83) + '|')
@@ -49,16 +49,27 @@ def login():
                 # Pause sebelum di clear
                 req = input("| Klik ENTER untuk melanjutkan... ")
                 core.clear()
+                main.mainmenu()
+                break
+            elif user['jenis_users_id'] == 2 and username == user['username'] and hashed_password == user['password']:  
+                salam = f"Selamat Datang Admin {username.title()}"
+                print('+' + '='*83 + '+')
+                print('|' + '[ LOGIN SUCCESSFUL ]'.center(83) + '|')
+                print('|' + salam.center(83) + '|')
+                print('+' + '='*83 + '+')
+                req = input("| Klik ENTER untuk melanjutkan... ")
+                core.clear()
+                main.menuadmin()
                 break
 
-            if username == user['username'] and hashed_password == user['password']:
-                print('yahaha gak bisa login')
+
+
         # -
-        if not sesi_login:
+        if not sesi_login and user['jenis_users_id'] != 1:
             # UI Login Gagal
             print('+' + '='*83 + '+')
             print('|' + '[ LOGIN FAILED ]'.center(83) + '|')
-            print('|' + 'Username atau Password salah!'.center(83) + '|')
+            print('|' + 'Username/Password salah atau bukan Owner!'.center(83) + '|')
             print('+' + '='*83 + '+')
             percobaan -= 1
             #  - 
--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import core
 import login
 import data_admin
 
-core.clear()
-login.login()
 
 def mainmenu():
     while True:
         core.clear()
         print('''
+[Owner Menu]
 1. Pendataan Admin
 2. Pendataan Penyewaan
 3. Pendatan Penghuni
@@ -46,7 +45,46 @@ def mainmenu():
                     core.clear()
             case _:
                 core.clear()
-    
+
+def menuadmin():
+    while True:
+        core.clear()
+        print('''
+[Admin Menu]
+1. Ubah Kondisi Fasilitas
+9. Kembali
+0. Keluar
+        ''')
+        menu = input("Mainmenu: ")
+        match menu:
+            case '1':
+                pass
+            case '9':
+                print('+' + '='*83 + '+')
+                print('|' + '[ NOTICE ]'.center(83) + '|')
+                print('|' + 'Apakah Anda yakin untuk kembali ke menu login? (y/n)'.center(83) + '|')
+                print('+' + '='*83 + '+')
+                user = input(f"| > ")
+                if user.lower() == 'y' or user.lower() == 'yes':
+                    core.clear()
+                    login.login() 
+                else:
+                    core.clear()
+            case '0':
+                print('+' + '='*83 + '+')
+                print('|' + '[ NOTICE ]'.center(83) + '|')
+                print('|' + 'Apakah Anda yakin untuk keluar? (y/n)'.center(83) + '|')
+                print('+' + '='*83 + '+')
+                user = input(f"| > ")
+                if user.lower() == 'y' or user.lower() == 'yes':
+                    core.clear()
+                    exit()
+                else:
+                    core.clear()
+            case _:
+                core.clear()
 
 if __name__ == "__main__":
+    core.clear()
+    login.login()
     mainmenu()
