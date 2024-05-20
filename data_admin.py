11600e54ececfaad4c32eaccfb0419780153f42c
ChemicalSpark <rangga.pramudya23@gmail.com>
2024-05-20 14:18:02 +0700

    menambah core dan mengupdate main

--- data_admin.py
+++ data_admin.py
@@ -1,75 +1,66 @@
 import connection
 import model
+import core
 
 
 def aksi_admin():
-    print('''
-1. Read
-2. Create
-3. Update
-4. Delete
-''')
-    admin = input("Masukkan nomor: ")
-    match admin:
-        case '1':
-           model.read_data(table="users",orderby="id_users")
+    while True:
+        core.clear()
+        print('''
+    1. Read
+    2. Create
+    3. Update
+    4. Delete
+    9. Kembali
+    ''')
+        admin = input("Masukkan nomor: ")
+        match admin:
+            case '1':
+                model.read_data(table="users",orderby="id_users")
 
-        case '2':
-            total_input = int(input(f"Berapa data yang ingin ditambahkan?: "))
-            for i in range(total_input):
-                nama_users = input("Masukkan nama lengkap: ")
-                username = input("Masukkan username: ")
-                password = input("Masukkan password: ")
-                no_telepon_users = input("Masukkan nomor telepon: ")
-                jenis_users_id = int(input("Masukkan jenis users: "))
-                query_admin = f"INSERT INTO users(nama_users, username, password, no_telepon_users, jenis_users_id) VALUES ('{nama_users}', '{username}', '{password}', '{no_telepon_users}', {jenis_users_id})"
-                connection.cursor.execute(query=query_admin, vars=(nama_users, username, password, no_telepon_users, jenis_users_id))
+            case '2':
+                model.create_data(table="users", values="")
 
-            connection.read_users(cursor=connection.cursor)
-            connection.conn.commit()
-            connection.cursor.close()
-            connection.conn.close()
+            case '3':
+                # update = f"UPDATE mata_kuliah SET nama_mata_kuliah = %s, sks = %s, semester_id = %s"
+                # read_matkul(cursor=cursor)
 
-        case '3':
-            # update = f"UPDATE mata_kuliah SET nama_mata_kuliah = %s, sks = %s, semester_id = %s"
-            # read_matkul(cursor=cursor)
+                id_users = input("Masukkan Id dari Users yang ingin di update: ")
+                select_query = f"SELECT * FROM users WHERE id_users = %s"
+                connection.cursor.execute(query=select_query, vars=(id_users))
+                data = connection.cursor.fetchone()
 
-            id_users = input("Masukkan Id dari Users yang ingin di update: ")
-            select_query = f"SELECT * FROM users WHERE id_users = %s"
-            connection.cursor.execute(query=select_query, vars=(id_users))
-            data = connection.cursor.fetchone()
+                if data:
+                    print("Data saat ini:")
+                    print(f"Id users saat ini: {data[0]}")
+                    print(f"Nama users saat ini: {data[1]}")
+                    print(f"Username saat ini: {data[2]}")
+                    print(f"Password saat ini: {data[3]}")
+                    print(f"Nomor telepon saat ini: {data[4]}")
+                    print(f"Jenis user saat ini: {data[5]}")
 
-            if data:
-                print("Data saat ini:")
-                print(f"Id users saat ini: {data[0]}")
-                print(f"Nama users saat ini: {data[1]}")
-                print(f"Username saat ini: {data[2]}")
-                print(f"Password saat ini: {data[3]}")
-                print(f"Nomor telepon saat ini: {data[4]}")
-                print(f"Jenis user saat ini: {data[5]}")
+                nama_users = input("Masukkan nama lengkap: ") or data[1]
+                username = input("Masukkan username: ") or data[2]
+                password = input("Masukkan password: ") or data[3]
+                no_telepon_users = input("Masukkan nomor telepon: ") or data[4]
+                jenis_users_id = int(input("Masukkan jenis users: ")) or data[5]
 
-            nama_users = input("Masukkan nama lengkap: ") or data[1]
-            username = input("Masukkan username: ") or data[2]
-            password = input("Masukkan password: ") or data[3]
-            no_telepon_users = input("Masukkan nomor telepon: ") or data[4]
-            jenis_users_id = int(input("Masukkan jenis users: ")) or data[5]
-
-            query_update = f"UPDATE users SET nama_users = %s, username = %s, password = %s WHERE id_users = %s"
-            connection.cursor.execute(query=query_update, vars=(nama_users, username, password, no_telepon_users, jenis_users_id))
+                query_update = f"UPDATE users SET nama_users = %s, username = %s, password = %s WHERE id_users = %s"
+                connection.cursor.execute(query=query_update, vars=(nama_users, username, password, no_telepon_users, jenis_users_id))
 
 
-            print(f"Total baris yang diubah: {connection.cursor.rowcount}")
-            connection.conn.commit()
-            connection.cursor.close()
-            connection.conn.close()
-        
-        case '4':
-            connection.read_users(cursor=connection.cursor)
-            id_users = input("Masukkan Id Users yang ingin di hapus: ")
-            query_delete = f"DELETE FROM users WHERE id_users = {id_users}"
-            connection.cursor.execute(query=query_delete)
+                print(f"Total baris yang diubah: {connection.cursor.rowcount}")
+                connection.conn.commit()
+                connection.cursor.close()
+                connection.conn.close()
+            
+            case '4':
+                connection.read_users(cursor=connection.cursor)
+                id_users = input("Masukkan Id Users yang ingin di hapus: ")
+                query_delete = f"DELETE FROM users WHERE id_users = {id_users}"
+                connection.cursor.execute(query=query_delete)
 
-            print(f"Total baris yang diubah: {connection.cursor.rowcount}")
-            connection.conn.commit()
-            connection.cursor.close()
-            connection.conn.close()
+                print(f"Total baris yang diubah: {connection.cursor.rowcount}")
+                connection.conn.commit()
+                connection.cursor.close()
+                connection.conn.close()
--- main.py
+++ main.py
@@ -1,17 +1,50 @@
+import core
 import data_admin
 
-print('''
-1. Tambah Admin
-2. Pendataan Penyewaan
-3. Pendatan Penghuni
-4. Pendataan Fasilitas
-5. Ubah Kondisi Fasilitas
-6. Pendataan Kamar
-9. Kembali
-0. Keluar
-''')
-mainmenu = input("Mainmenu: ")
-match mainmenu:
-    case '1':
-        data_admin.aksi_admin()
+
+def mainmenu():
+    while True:
+        core.clear()
+        print('''
+        1. Tambah Admin
+        2. Pendataan Penyewaan
+        3. Pendatan Penghuni
+        4. Pendataan Fasilitas
+        5. Ubah Kondisi Fasilitas
+        6. Pendataan Kamar
+        9. Kembali
+        0. Keluar
+        ''')
+        menu = input("Mainmenu: ")
+        match menu:
+            case '1':
+                data_admin.aksi_admin()
+            case '9':
+                print('+' + '='*83 + '+')
+                print('|' + '[ NOTICE ]'.center(83) + '|')
+                print('|' + 'Apakah Anda yakin untuk kembali ke menu login? (y/n)'.center(83) + '|')
+                print('+' + '='*83 + '+')
+                user = input(f"| > ")
+                if user.lower() == 'y' or user.lower() == 'yes':
+                    core.clear()
+                    continue
+                    #login.login() 
+                else:
+                    core.clear()
+            case '0':
+                print('+' + '='*83 + '+')
+                print('|' + '[ NOTICE ]'.center(83) + '|')
+                print('|' + 'Apakah Anda yakin untuk keluar? (y/n)'.center(83) + '|')
+                print('+' + '='*83 + '+')
+                user = input(f"| > ")
+                if user.lower() == 'y' or user.lower() == 'yes':
+                    core.clear()
+                    exit()
+                else:
+                    core.clear()
+            case _:
+                core.clear()
     
+
+if __name__ == "__main__":
+    mainmenu()
