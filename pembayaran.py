d589cdaf16b9de35e485193fe500688547c168fb
oujirate <oujireflect.me@gmail.com>
2024-05-22 11:32:34 +0700

    hapus pembayaran

--- pembayaran.py
+++ pembayaran.py
@@ -25,7 +25,24 @@ def update_pembayaran():
     pass
 
 def hapus_pembayaran():
-    pass
+    read_pembayaran()
+    kolom = input("Pilih ID yang ingin dihapus: ")
+    data = model.read_data(table=tabel,columnid=kolom)
+    data2 = model.read_data(table=tabel,columnid=data[3])
+    column = model.column_data(table=tabel,idenable=True)
+    x = print("[Data yang ingin dihapus]")
+    print(f"ID Pembayaran: {data[0]}")
+    print(f"Tanggal Pembayaran: {data[1]}")
+    print(f"Tenggat Pembayaran: {data[2]}")
+    print(f"Penghuni_id: {data2[1]}")
+    req = input("Apakah anda yakin ingin menghapus? (Y/n)")
+    if req == 'Y' or req == 'y':nonloca
+        model.delete_data(table=tabel,idcolumn=kolom)
+    else:
+        req = input("Data gagal dihapus!")
+
+    
+
 
 def aksi_pembayaran():
     menambah_pembayaran()
@@ -33,4 +50,4 @@ def aksi_pembayaran():
 if __name__ == "__main__":
     # column = model.column_data(table=tabel)
     # print(column)
-    aksi_pembayaran()
+    hapus_pembayaran()
