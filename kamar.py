3e89b2cc19a605a06ae6b8517f207244b664600c
ChemicalSpark <rangga.pramudya23@gmail.com>
2024-05-26 12:39:21 +0700

    menambahkan pengecekan untuk kamar dan penghuni

--- kamar.py
+++ kamar.py
@@ -4,86 +4,217 @@ import main
 import connection
 
 tabel_kamar="kamar"
+tabel_penghuni="penghuni"
+
+def read_join(columnid=""):
+    conn, cur = connection.connect()
+    column = model.column_data(table=tabel_kamar, idenable=1)
+
+    query_join = f"""
+SELECT k.id_kamar, k.nomor_kamar, tk.nama_tipe_kamar
+FROM kamar k
+JOIN tipe_kamar tk ON tk.id_tipe_kamar = k.tipe_kamar_id
+"""
+    
+    # if columnid:
+    #     query = f"{query_join} WHERE {column[0]} = {columnid}"
+    #     cur.execute(query, (columnid))
+    #     result = cur.fetchone()
+    # else:
+    query = f"{query_join} ORDER BY id_kamar ASC"
+    cur.execute(query)
+    result = cur.fetchall()
+        
+    cur.close()
+    conn.close()
+    return result
 
 def baca_kamar ():
-    data = model.column_data(table=tabel_kamar,idenable=True)
-    read = model.read_data(table=tabel_kamar)
-    print(data)
-    for i in read:
-        print(i)
+    # data = model.column_data(table=tabel_kamar,idenable=True)
+    read = read_join()
+    # print(data)
+    if read:
+        print(f"{'ID':<5} {'Nomor Kamar':<12} {'Tipe Kamar':<10}")
+        print("-" * 36)
+        for row in read:
+            id_kamar, nomor_kamar, tipe_kamar_id = row
+            print(f"{id_kamar:<5} {nomor_kamar:<12} {tipe_kamar_id:<10}")
+    else:
+        print("[Tidak ada data kamar yang tersedia]")
+
+    req = input("Klik ENTER untuk kembali...")
 
 def tambah_kamar ():
-    nama_kamar = input ("Masukkan Nomer Kamar: ")
-    data_kamar = model.read_data(table="kamar")
-    for i in data_kamar:
-        print(i)
-    print("ID 1 = Kamar Mandi Dalam\nID 2 = Kamar Mandi Luar")
-    tipe_kamar_id = input ("masukkan ID Tipe Kamar: ")
-    data_kamar = model.read_data(table="kamar")
-    values = [nama_kamar,tipe_kamar_id]
-    model.create_data(tabel_kamar,values)\
-    
+    while True:
+        nomor_kamar = input ("Masukkan Nomor Kamar: ")
+        data_kamar = model.read_data(table="kamar")
+        for i in data_kamar:
+            print(i)
+        print("ID 1 = Kamar Mandi Dalam\nID 2 = Kamar Mandi Luar")
+        tipe_kamar_id = input ("masukkan ID Tipe Kamar: ")
+        if not (nomor_kamar and tipe_kamar_id):
+            print('\n[ DATA TIDAK LENGKAP ]')
+            print('Klik ENTER untuk melanjutkan!')
+            enter = input()
+            core.clear()
+            break
+
+        data = model.read_data(table="kamar")
+        data_ada = [cek[1] for cek in data]
+        # for cek in data[1:]:
+        #     data_ada.append(cek[1])
+        if nomor_kamar in data_ada:
+            print('\n[ KAMAR DENGAN NOMOR INI SUDAH ADA ]')
+            print('Klik ENTER untuk melanjutkan!')
+            enter = input()
+            core.clear()
+            break
+
+        values = [nomor_kamar,tipe_kamar_id]
+        model.create_data(tabel_kamar,values)
+        print("\n[Data kamar sudah ditambahkan]")
+        req = input('Klik ENTER untuk melanjutkan!')
+        core.clear()
+        break
+
 def ubah_kamar ():
-    data = model.column_data(table=tabel_kamar,idenable=True)
-    read = model.read_data(table=tabel_kamar)
-    print(data)
-    for i in read:
-        print(i)
-    pilih_id = int(input("Masukan ID Kamar : "))
-    print("Data yang anda pilih : ")
-    read = model.read_data(table=tabel_kamar, columnid=pilih_id)
-    print(f"ID Kamar   : {read[0]}")
-    print(f"Nomer Kamar: {read[1]}")
-    print(f"Tipe Kamar : {read[2]}")
-    print("== Masukkan Data Baru ==")
-    nomor_kamar = input("Nomer Kamar Baru : ") or read[1]
-    print("ID = 1 Kamar Mandi Dalam\nID 2 = Kamar Mandi Luar")
-    tipe_kamar = int(input("ID Tipe Kamar Baru (1/2): ") or read[2])
-    values = [nomor_kamar,tipe_kamar]
-    model.update_data(tabel_kamar, pilih_id,values)
+    while True:
+        # data = model.column_data(table=tabel_kamar,idenable=True)
+        read = read_join()
+        # print(data)
+        if read:
+            print(f"{'ID':<5} {'Nomor Kamar':<12} {'Tipe Kamar':<10}")
+            print("-" * 36)
+            for row in read:
+                id_kamar, nomor_kamar, tipe_kamar_id = row
+                print(f"{id_kamar:<5} {nomor_kamar:<12} {tipe_kamar_id:<10}")
+        else:
+            print("[Tidak ada data kamar yang tersedia]")
+            req = input('Klik ENTER untuk melanjutkan!')
+            core.clear()
+            break
+
+        pilih_id = input("Masukan ID Kamar : ")
+        if pilih_id:
+            try:    
+                pilih_id = int(pilih_id)
+                if any(id_kamar == pilih_id for id_kamar, _, _ in read):
+                    print("Data yang anda pilih : ")
+                    read_column = model.read_data(table=tabel_kamar, columnid=pilih_id)
+                    print(f"ID Kamar   : {read_column[0]}")
+                    print(f"Nomer Kamar: {read_column[1]}")
+                    print(f"Tipe Kamar : {read_column[2]}")
+                    print("== Masukkan Data Baru ==")
+                    nomor_kamar = input("Nomer Kamar Baru : ") or read[1]
+                    print("ID = 1 Kamar Mandi Dalam\nID 2 = Kamar Mandi Luar")
+                    tipe_kamar = int(input("ID Tipe Kamar Baru (1/2): ") or read[2])
+                    values = [nomor_kamar,tipe_kamar]
+                    model.update_data(tabel_kamar, pilih_id,values)
+                    print("\n[Data kamar sudah diperbarui]")
+                    req = input('Klik ENTER untuk melanjutkan!')
+                    core.clear()
+                    break
+                else:
+                    print("\n[Data tidak ada!]")
+                    req = input('Klik ENTER untuk melanjutkan!')
+                    core.clear()
+                    continue
+            except ValueError:
+                print("\n[Id harus berupa angka saja!]")
+                req = input('Klik ENTER untuk melanjutkan!')
+                core.clear()
+                continue
+
+        else:
+            core.clear()
+            continue
+        
 
 def hapus_kamar():
-    data = model.column_data(table=tabel_kamar,idenable=True)
-    read = model.read_data(table=tabel_kamar)
-    for i in read:
-        print(i)
-    pilih_id = int(input("Pilih ID data (Nomor): "))
-    user = input("Yakin ingin menghapus?(Y/n) ")
-    if user.lower() == 'y':
-        model.delete_data(table=tabel_kamar,idcolumn=pilih_id)
-    else:
-        print("Data tidak jadi dihapus")
+    while True:
+        # data = model.column_data(table=tabel_kamar,idenable=True)
+        read = read_join()
+        if read:
+            print(f"{'ID':<5} {'Nomor Kamar':<12} {'Tipe Kamar':<10}")
+            print("-" * 36)
+            for row in read:
+                id_kamar, nomor_kamar, tipe_kamar = row
+                print(f"{id_kamar:<5} {nomor_kamar:<12} {tipe_kamar:<10}")
+        else:
+            print("[Tidak ada data kamar yang tersedia]")
+            req = input('Klik ENTER untuk melanjutkan!')
+            core.clear()
+            break
+
+        pilih_id = input("Pilih ID data (Nomor): ")
+        if pilih_id:
+            try:    
+                pilih_id = int(pilih_id)
+                if any(id_kamar == pilih_id for id_kamar, _, _ in read):
+                    user = input("Yakin ingin menghapus?(Y/n) ")
+                    if user.lower() == 'y':
+                        model.delete_data(table=tabel_kamar,idcolumn=pilih_id,foreign_key="kamar_id",foreign_table=tabel_penghuni)
+                        print('\n[Data kamar sudah dihapus]')
+                        if "kamar_id" == True:
+                            print(f'[Menghapus data yang menggunakan foreign key dari tabel {tabel_penghuni}]')
+                        req = input('Klik ENTER untuk melanjutkan!')
+                        core.clear()
+                        break
+                    else:
+                        print("\n[Data kamar tidak jadi dihapus]")
+                        req = input('Klik ENTER untuk melanjutkan!')
+                        core.clear()
+                        break
+
+                else:
+                    print("\n[Data tidak ada!]")
+                    req = input('Klik ENTER untuk melanjutkan!')
+                    core.clear()
+                    continue
+            except ValueError:
+                print("\n[Id harus berupa angka saja!]")
+                req = input('Klik ENTER untuk melanjutkan!')
+                core.clear()
+                continue
+
+        else:
+            core.clear()
+            continue
+
+        
 # def tambah_kamar()
 def aksi_kamar():
     core.clear()
     while True:
         print(" [ KAMAR ]")
-        print("""| Menu:
-    1. Menambah data kamar
-    2. Data kamar
-    3. Edit data kamar
-    4. Hapus data kamar 
-    5. Kembali
-    """)   
+        print("""
+| Menu:
+1. Lihat data kamar
+2. Menambah data kamar
+3. Perbarui data kamar
+4. Hapus data kamar 
+9. Kembali
+""")   
         input_usr = input("|> Pilih Menu: ")
         
         match input_usr :
-            case "1":
-                core.clear
-                tambah_kamar()
-            case "2": 
+            case "1": 
                 core.clear
                 baca_kamar()
+            case "2":
+                core.clear
+                tambah_kamar()
             case "3":
                 core.clear()
                 ubah_kamar()
             case "4":
                 core.clear()
                 hapus_kamar()
-            case "5":
+            case "9":
                 core.clear()
                 main.mainmenu()
 
 
 
-
+if __name__ == "__main__":
+    aksi_kamar()
--- penghuni.py
+++ penghuni.py
@@ -4,20 +4,48 @@ import core
 table = "penghuni"
 
 def new_penghuni():
-    nik_penghuni = input("| Masukkan NIK\t: ")
-    nama_penghuni = input("| Masukkan Nama Lengkap\t: ")
-    no_telepon_penghuni = input("| Nomor Telepon\t: ")
-    print("Format Tanggal Masuk = yyyy-mm-dd hh:mm:ss")
-    tanggal_masuk = input("| Tanggal Masuk\t: ")
-    tanggal_keluar = None
-    data_kamar = model.read_data(table="kamar")
-    for i in data_kamar:
-        print(i)
-    kamar_id = input("| Kamar\t: ")
+    while True:
+        nik_penghuni = input("| Masukkan NIK\t: ")
+        nama_penghuni = input("| Masukkan Nama Lengkap\t: ")
+        no_telepon_penghuni = input("| Nomor Telepon\t: ")
+        print("Format Tanggal Masuk = yyyy-mm-dd hh:mm:ss")
+        tanggal_masuk = input("| Tanggal Masuk\t: ")
+        tanggal_keluar = input("| Tanggal Keluar(opsional\t: ")
+        data_kamar = model.read_data(table="kamar")
+        for i in data_kamar:
+            print(i)
+        kamar_id = input("| Kamar\t: ")
 
-    values = [nik_penghuni,nama_penghuni,no_telepon_penghuni,tanggal_masuk,tanggal_keluar,kamar_id]
+        if not (nik_penghuni and nama_penghuni and no_telepon_penghuni and tanggal_masuk and kamar_id):
+            print('\n[ DATA TIDAK LENGKAP ]')
+            print('Klik ENTER untuk melanjutkan!')
+            enter = input()
+            core.clear()
+            break
+
+        data = model.read_data(table=table)
+        data_ada = [cek[1] for cek in data]
+        # for cek in data[1:]:
+        #     data_ada.append(cek[1])
+        if nik_penghuni in data_ada:
+            print('\n[ PENGHUNI DENGAN NIK INI SUDAH ADA ]')
+            print('Klik ENTER untuk melanjutkan!')
+            enter = input()
+            core.clear()
+            break
+
+
+        if tanggal_keluar:
+            values = [nik_penghuni,nama_penghuni,no_telepon_penghuni,tanggal_masuk,tanggal_keluar,kamar_id]
+        else:
+            values = [nik_penghuni,nama_penghuni,no_telepon_penghuni,tanggal_masuk,None,kamar_id]
+
+        model.create_data(table=table,values=values)
+        print("\n[Data penghuni sudah ditambahkan]")
+        req = input('Klik ENTER untuk melanjutkan!')
+        core.clear()
+        break
     
-    model.create_data(table=table,values=values)
 
 def data_penghuni():
     # column = model.column_data(table=table, idenable=True)
@@ -32,90 +60,155 @@ def data_penghuni():
             id_penghuni, nik_penghuni, nama_penghuni, no_telepon_penghuni, tanggal_masuk, tanggal_keluar, kamar_id = row
             print(f"{id_penghuni:<5} {nik_penghuni:<20} {nama_penghuni:<30} {no_telepon_penghuni:<25} {str(tanggal_masuk):<25} {str(tanggal_keluar):<20} {kamar_id:<5}")
     else:
-        print("Tidak ada data kondisi fasilitas yang tersedia.")
+        print("[Tidak ada data penghuni yang tersedia]")
     req = input("")
 
 def edit_penghuni():
-    # kolom = model.column_data(table=table, idenable=True)
-    data = model.read_data(table=table,orderby="id_penghuni")
-    data_kamar = model.read_data(table="kamar")
-    # print(kolom)
-    if data:
-        print(f"{'ID':<5} {'NIK Penghuni':<20} {'Nama Penghuni':<30} {'Nomor Telepon Penghuni':<25} {'Tanggal Masuk':<25} {'Tanggal keluar':<20} {'Kamar ID':<5}")
-        print("-" * 140)
-        for row in data:
-            id_penghuni, nik_penghuni, nama_penghuni, no_telepon_penghuni, tanggal_masuk, tanggal_keluar, kamar_id = row
-            print(f"{id_penghuni:<5} {nik_penghuni:<20} {nama_penghuni:<30} {no_telepon_penghuni:<25} {str(tanggal_masuk):<25} {str(tanggal_keluar):<20} {kamar_id:<5}")
-    else:
-        print("Tidak ada data kondisi fasilitas yang tersedia.")
-    pilih_id = int(input("Pilih Data (Nomor) : "))
-    print("Data yang anda pilih : ")
-    data = model.read_data(table=table, columnid=pilih_id)
-    print("NIK Penghuni : "+ data[1])
-    print("Nama Penghuni: "+ data[2])
-    print("Nomor Telepon: "+ data[3])
-    print("Tanggal Masuk: "+ str(data[4]))
-    print("Tanggal Keluar: "+ str(data[5]))
-    data_kamar_penghuni = model.read_data(table="kamar",columnid=pilih_id)
-    print("Nomor Kamar: "+ data_kamar_penghuni[1])
-    print("== Masukkan Data Baru ==")
-    nik_penghuni = input("Masukkan NIK (Enter untuk skip) : ") or data[1]
-    nama_penghuni = input("Masukkan Nama Lengkap (Enter untuk skip) : ") or data[2]
-    no_telepon_penghuni = input("Masukkan No telepon penghuni: ") or data[3]
-    tanggal_masuk = input("Masukkan tanggal masuk dengan format YYYY-MM-DD HH:MM:SS: ") or str(data[4])
-    tanggal_keluar = input("Masukkan tanggal keluar dengan format YYYY-MM-DD HH:MM:SS (Opsional): ") or str(data[5])
-    for i in data_kamar:
-        print(i)
-    kamar_id = input("Masukkan ID Kamar: ") or data[6]
+    while True:
+        # kolom = model.column_data(table=table, idenable=True)
+        data = model.read_data(table=table,orderby="id_penghuni")
+        data_kamar = model.read_data(table="kamar")
+        # print(kolom)
+        if data:
+            print(f"{'ID':<5} {'NIK Penghuni':<20} {'Nama Penghuni':<30} {'Nomor Telepon Penghuni':<25} {'Tanggal Masuk':<25} {'Tanggal keluar':<20} {'Kamar ID':<5}")
+            print("-" * 140)
+            for row in data:
+                id_penghuni, nik_penghuni, nama_penghuni, no_telepon_penghuni, tanggal_masuk, tanggal_keluar, kamar_id = row
+                print(f"{id_penghuni:<5} {nik_penghuni:<20} {nama_penghuni:<30} {no_telepon_penghuni:<25} {str(tanggal_masuk):<25} {str(tanggal_keluar):<20} {kamar_id:<5}")
+        else:
+            print("[Tidak ada data penghuni yang tersedia]")
+        pilih_id = input("Pilih Data (Nomor) : ")
+        if pilih_id:
+            try:    
+                pilih_id = int(pilih_id)
+                if any(id_penghuni == pilih_id for id_penghuni, _, _, _, _, _, _ in data):
+                    print("Data yang anda pilih : ")
+                    data_column = model.read_data(table=table, columnid=pilih_id)
+                    print("NIK Penghuni : "+ data_column[1])
+                    print("Nama Penghuni: "+ data_column[2])
+                    print("Nomor Telepon: "+ data_column[3])
+                    print("Tanggal Masuk: "+ str(data_column[4]))
+                    print("Tanggal Keluar: "+ str(data_column[5]))
+                    data_kamar_penghuni = model.read_data(table="kamar",columnid=pilih_id)
+                    print("Nomor Kamar: "+ data_kamar_penghuni[1])
+                    print("== Masukkan Data Baru ==")
+                    nik_penghuni = input("Masukkan NIK (Enter untuk skip) : ") or data[1]
+                    nama_penghuni = input("Masukkan Nama Lengkap (Enter untuk skip) : ") or data[2]
+                    no_telepon_penghuni = input("Masukkan No telepon penghuni: ") or data[3]
+                    tanggal_masuk = input("Masukkan tanggal masuk dengan format YYYY-MM-DD HH:MM:SS: ") or str(data[4])
+                    tanggal_keluar = input("Masukkan tanggal keluar dengan format YYYY-MM-DD HH:MM:SS (Opsional): ") or str(data[5])
+                    for i in data_kamar:
+                        print(i)
+                    kamar_id = input("Masukkan ID Kamar: ") or data[6]
 
-    values = [nik_penghuni,nama_penghuni,no_telepon_penghuni,tanggal_masuk,tanggal_keluar,kamar_id]
-    model.update_data(table, pilih_id, values)
+                    if tanggal_keluar:
+                        values = [nik_penghuni,nama_penghuni,no_telepon_penghuni,tanggal_masuk,tanggal_keluar,kamar_id]
+                    else:
+                        values = [nik_penghuni,nama_penghuni,no_telepon_penghuni,tanggal_masuk,None,kamar_id]
+
+                    model.update_data(table, pilih_id, values)
+                    print("\n[Data penghuni sudah diperbarui]")
+                    req = input('Klik ENTER untuk melanjutkan!')
+                    core.clear()
+                    break
+                    
+                else:
+                    print("\n[Data tidak ada!]")
+                    req = input('Klik ENTER untuk melanjutkan!')
+                    core.clear()
+                    continue
+            except ValueError:
+                print("\n[Id harus berupa angka saja!]")
+                req = input('Klik ENTER untuk melanjutkan!')
+                core.clear()
+                continue
+
+        else:
+            core.clear()
+            continue
+        
 
 def hapus_penghuni():
-    read = model.read_data(table=table,orderby="id_penghuni")
-    if read:
-        print(f"{'ID':<5} {'NIK Penghuni':<20} {'Nama Penghuni':<30} {'Nomor Telepon Penghuni':<25} {'Tanggal Masuk':<25} {'Tanggal keluar':<20} {'Kamar ID':<5}")
-        print("-" * 140)
-        for row in read:
-            id_penghuni, nik_penghuni, nama_penghuni, no_telepon_penghuni, tanggal_masuk, tanggal_keluar, kamar_id = row
-            print(f"{id_penghuni:<5} {nik_penghuni:<20} {nama_penghuni:<30} {no_telepon_penghuni:<25} {str(tanggal_masuk):<25} {str(tanggal_keluar):<20} {kamar_id:<5}")
-    else:
-        print("Tidak ada data kondisi fasilitas yang tersedia.")
-    id_column = int(input(f"pilih ID {table} yang akan dihapus: "))
-    read_column = model.read_data(table=table,columnid=id_column)
-    print(f"ID Penghuni: {read_column[0]}")
-    print(f"NIK Penghuni: {read_column[1]}")
-    print(f"Nama Penghuni: {read_column[2]}")
-    user = input("Yakin ingin menghapus?(Y/n) ")
-    if user.lower() == 'y':
-        model.delete_data(table=table,idcolomn=id_column)
-    else:
-        print("Data tidak jadi dihapus")
+    while True:
+        read = model.read_data(table=table,orderby="id_penghuni")
+        if read:
+            print(f"{'ID':<5} {'NIK Penghuni':<20} {'Nama Penghuni':<30} {'Nomor Telepon Penghuni':<25} {'Tanggal Masuk':<25} {'Tanggal keluar':<20} {'Kamar ID':<5}")
+            print("-" * 140)
+            for row in read:
+                id_penghuni, nik_penghuni, nama_penghuni, no_telepon_penghuni, tanggal_masuk, tanggal_keluar, kamar_id = row
+                print(f"{id_penghuni:<5} {nik_penghuni:<20} {nama_penghuni:<30} {no_telepon_penghuni:<25} {str(tanggal_masuk):<25} {str(tanggal_keluar):<20} {kamar_id:<5}")
+        else:
+            print("[Tidak ada data penghuni yang tersedia]")
+        id_column = input(f"pilih ID {table} yang akan dihapus: ")
+        if id_column:
+            try:    
+                id_column = int(id_column)
+                if any(id_penghuni == id_column for id_penghuni, _, _, _, _, _, _ in read):
+                    read_column = model.read_data(table=table,columnid=id_column)
+                    print(f"ID Penghuni: {read_column[0]}")
+                    print(f"NIK Penghuni: {read_column[1]}")
+                    print(f"Nama Penghuni: {read_column[2]}")
+                    user = input("Yakin ingin menghapus?(Y/n) ")
+                    if user.lower() == 'y':
+                        model.delete_data(table=table,idcolumn=id_column)
+                        print('\n[Data penghuni sudah dihapus]')
+                        req = input('Klik ENTER untuk melanjutkan!')
+                        core.clear()
+                        break
+                    else:
+                        print("\n[Data penghuni tidak jadi dihapus]")
+                        req = input('Klik ENTER untuk melanjutkan!')
+                        core.clear()
+                        break
+                    
+                else:
+                    print("\n[Data tidak ada!]")
+                    req = input('Klik ENTER untuk melanjutkan!')
+                    core.clear()
+                    continue
+            except ValueError:
+                print("\n[Id harus berupa angka saja!]")
+                req = input('Klik ENTER untuk melanjutkan!')
+                core.clear()
+                continue
 
-    read = model.read_data(table=table)
-    for i in read:
-        print(i)
+        else:
+            core.clear()
+            continue
+        
+
+        # read = model.read_data(table=table)
+        # for i in read:
+        #     print(i)
 
 def aksi_penghuni():
     while True:
         core.clear()
         print(" [PENGHUNI]")
-        print("""| Menu:
-    1. Menambah data penghuni
-    2. Lihat Data penghuni
-    3. Perbarui data penghuni
-    4. Hapus dana penghuni 
+        print("""
+| Menu:
+1. Lihat Data penghuni
+2. Menambah data penghuni
+3. Perbarui data penghuni
+4. Hapus dana penghuni 
+9. Kembali
     """)   
         input_usr = input("|> Pilih Menu: ")
         if input_usr == '1':
-            core.clear()
-            new_penghuni()
-        elif input_usr == '2':
             data_penghuni()
+            core.clear()
+        elif input_usr == '2':
+            new_penghuni()
+            core.clear()
         elif input_usr == '3':
             edit_penghuni()
+            core.clear()
         elif input_usr == '4':
             hapus_penghuni()
+            core.clear()
+        elif input_usr == '9':
+            core.clear()
+            break
         else:
             req = input("")
             core.clear()
