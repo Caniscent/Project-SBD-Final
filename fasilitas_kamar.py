02ea315aecb68ab52c423b1d886673505cfcef36
oujirate <oujireflect.me@gmail.com>
2024-05-29 15:16:00 +0700

    CRUD FK

--- fasilitas_kamar.py
+++ fasilitas_kamar.py
@@ -24,7 +24,6 @@ def CreateFK():
 
 def ReadFK():
     try:
-        conn, cur = connection.connect()
         data = model.read_data(select=f"{TabelFK}.{ColumnFK[0]},{TabelKamar}.{ColumnKamar[1]},{TabelFasilitas}.{ColumnFasilitas[1]}",
                                table=TabelFK,
                                join_tables=[TabelKamar,TabelFasilitas],
@@ -34,25 +33,39 @@ def ReadFK():
             print(i)
     except Exception as e:
         print(f"Gagal membaca data! Error: {e}")
-    finally:
-        cur.close()
-        conn.close()
 
 def UpdateFK():
     try:
-        pass
-    except:
-        pass
-    finally:
-        pass
+        ReadFK()
+        id_fk = input("Masukkan ID Fasilitas Kamar: ")
+        dataFK = model.read_data(select=f"{TabelFK}.{ColumnFK[0]},{TabelKamar}.{ColumnKamar[1]},{TabelFasilitas}.{ColumnFasilitas[1]}",
+                               table=TabelFK,
+                               join_tables=[TabelKamar,TabelFasilitas],
+                               join_conditions=[f"{TabelKamar}.{ColumnKamar[0]} = {TabelFK}.{ColumnFK[1]}",
+                                                f"{TabelFasilitas}.{ColumnFasilitas[0]} = {TabelFK}.{ColumnFK[2]}"],
+                                columnid=id_fk)
+        print("Data saat ini: ")
+        print(f"ID Fasilitas Kamar: {dataFK[0]}")
+        print(f"Nonor Kamar: {dataFK[1]}")
+        print(f"Fasilitas: {dataFK[2]}")
+        print("\nUbah Data: ")
+        data_kamar = model.read_data(table=TabelKamar)
+        for i in data_kamar:
+            print (i)
+        nomor_kamar = input(f"Masukkan Nomor Kamar: ") or dataFK[1]
+        nama_fasilitas = input(f"Masukkan Nama Fasilitas: ") or dataFK[2]
+        values = [nomor_kamar,nama_fasilitas]
+        model.update_data(table=TabelFK,idcolomn=id_fk,values=values)
+    except Exception as ex:
+        print(f"Error: {ex}")
 
 def DeleteFK():
     try:
-        pass
-    except:
-        pass
-    finally:
-        pass
+        ReadFK()
+        req = input("Pilih ID yang ingin dihapus: ")
+        model.delete_data(table=TabelFK,idcolumn=req)
+    except Exception as ex:
+        print(f"Error: {ex}")
 
 def AksiFasilitasKamar():
     try:
